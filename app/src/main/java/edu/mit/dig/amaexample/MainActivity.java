c311515e8c5ba7261a8df564d32508351451c4a6
Aaron Vontell <vontell@mit.edu>
2016-10-22 17:12:39 -0400

    Initial lib commit

---
+++ app/src/main/java/edu/mit/dig/amaexample/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.mit.dig.amaexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

364b2bb6fb0693b577a2be20ef11f2b9d94452da
Aaron Vontell <vontell@mit.edu>
2016-10-22 17:53:47 -0400

    Implement TalkBack installed features, fixes #1

---
+++ ama/src/main/java/edu/mit/dig/ama/vision/Features.java
@@ -0,0 +1,80 @@
+package edu.mit.dig.ama.vision;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.view.accessibility.AccessibilityManager;
+
+import java.util.List;
+
+import static android.content.Context.ACCESSIBILITY_SERVICE;
+
+/**
+ * Class with methods for limited vision accessibility features
+ * @author Aaron Vontell, Billy Caruso, Byungkyu Park
+ * @version 10.22.16
+ */
+public class Features {
+
+    private static final String TALKBACK_PACKAGE = "com.google.android.marvin.talkback";
+
+    /**
+     * Checks to see if TalkBack is installed (note that this is the Google version of TalkBack)
+     * @param context The calling activity
+     * @return true if TalkBack is installed on this device
+     */
+    static boolean isTalkBackInstalled(Context context) {
+
+        List<ApplicationInfo> packages;
+        PackageManager pm;
+
+        pm = context.getPackageManager();
+        packages = pm.getInstalledApplications(0);
+        for (ApplicationInfo packageInfo : packages) {
+            if(packageInfo.packageName.equals(TALKBACK_PACKAGE))
+                return true;
+        }
+        return false;
+
+    }
+
+    /**
+     * Checks to see if TalkBack is currently enabled
+     * @param context The calling activity
+     * @return true if TalkBack is enabled
+     */
+    static boolean isTalkBackEnabled(Context context) {
+
+        AccessibilityManager am = (AccessibilityManager) context.getSystemService(ACCESSIBILITY_SERVICE);
+        return am.isEnabled();
+
+    }
+
+    /**
+     * Checks to see if explore by touch (provided via TalkBack) is currently enabled
+     * @param context The calling activity
+     * @return true if explore by touch is enabled
+     */
+    static boolean isExploreByTouchEnabled(Context context) {
+
+        AccessibilityManager am = (AccessibilityManager) context.getSystemService(ACCESSIBILITY_SERVICE);
+        return am.isTouchExplorationEnabled();
+
+    }
+
+    /**
+     * Returns an Intent that will open Google Play Store, and link to the TalkBack app page
+     * @return The intent that will open the play store at the TalkBack page
+     */
+    static Intent getTalkBackPlayIntent() {
+
+        Intent talkbackIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("market://details?id=" + TALKBACK_PACKAGE));
+
+        return talkbackIntent;
+
+    }
+
+}

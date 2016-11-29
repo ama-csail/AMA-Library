2b5f37dec34bc02dd473f9580b7ef9c21f01e8a1
William Caruso <wcaruso.mit@gmail.com>
2016-11-28 20:28:14 -0500

    Added feature defs for MotorSkills and Limited Cognition

--- ama/src/main/java/edu/mit/dig/ama/features/Cognition.java
+++ ama/src/main/java/edu/mit/dig/ama/features/Cognition.java
@@ -1,8 +1,154 @@
 package edu.mit.dig.ama.features;
 
 /**
- * Created by Vontell on 11/28/16.
+ * Limited Cognition related accessibility helper functions.
  */
 
 public class Cognition {
+
+    /**
+     *
+     * Sets the HelpMessage of a View
+     * @param view The view to set the HelpMessage on
+     * @param class The String HelpMessage to set
+     */
+    public void setHelpMessage(View view, String helpMessage){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Gets the HelpMessage of a View
+     * @param view The view to get the HelpMessage on
+     * @return The String HelpMessage of the view
+     */
+    public String getHelpMessage(View view){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets the ActionClass of a View
+     * @param view The view to set the ActionClass on
+     * @param actionClass The ActionClass to set, which can be one of
+     *                 - ActionClass.DEFAULT
+     *                 - ActionClass.PRIMARY
+     *                 - ActionClass.SUCCESS
+     *                 - ActionClass.INFO
+     *                 - ActionClass.WARNING
+     *                 - ActionClass.DANGER
+     *                 - ActionClass.LINK
+     */
+    public void setActionClass(View view, ActionClass actionClass){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Gets the ActionClass of a View
+     * @param view The view to set the ActionClass on
+     * @return class The ActionClass to set, which can be one of
+     *                 - ActionClass.DEFAULT
+     *                 - ActionClass.PRIMARY
+     *                 - ActionClass.SUCCESS
+     *                 - ActionClass.INFO
+     *                 - ActionClass.WARNING
+     *                 - ActionClass.DANGER
+     *                 - ActionClass.LINK
+     */
+    public ActionClass getActionClass(View view){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+
+
+    /**
+     *
+     * Gets the number of Views on an Activity
+     * @param activity The activity to check
+     * @return the number of views
+     */
+    public int getNumViews(Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Gets all the Views on an Activity
+     * @param activity The activity to check
+     * @return an ArrayList of views
+     */
+    public ArrayList<View> getAllViews(Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Gets all the Strings associated with an Activity
+     * @param activity The activity to check
+     * @return an ArrayList of Strings
+     */
+    public ArrayList<String> getAllStrings(Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Gets all the complex Strings associated with an Activity
+     * @param activity The activity to check
+     * @return an ArrayList of complex Strings
+     */
+    public ArrayList<String> getAllComplexStrings(Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets all the simple Strings associated with complex Strings in an
+     * Activity
+     * @param an Map of complex Strings and simple strings
+     */
+    public void setSimpleStringAlternatives(HashMap strings){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets action feedback for a View
+     * @param view The view to set
+     * @param feedbackMessage String to set
+     */
+    public void setFeedbackMessage(View view, String feedbackMessage){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Gets action feedback for a View
+     * @param view The view to get the feedbackMessage from
+     */
+    public void getFeedbackMessage(View view){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Gets bulleted version of long string
+     * @param paragraph The String to break down
+     * @return bulleted version of paragraph
+     */
+    public String getBullettedString(String paragraph){
+        throw new RuntimeException("Method not implemented");
+    }
 }
--- ama/src/main/java/edu/mit/dig/ama/features/MotorSkills.java
+++ ama/src/main/java/edu/mit/dig/ama/features/MotorSkills.java
@@ -1,8 +1,110 @@
 package edu.mit.dig.ama.features;
 
 /**
- * Created by Vontell on 11/28/16.
+ * MotorSkills related accessibility helper functions.
  */
 
 public class MotorSkills {
+
+    /**
+     *
+     * Sets the NavType of an Activity
+     * @param activity The Activity to set the NavType on
+     * @param type The NavType to set
+     *                 - NavType.TABS
+     *                 - NavType.TABS+SWIPE
+     *                 - NavType.SWIPE+TITLE
+     *                 - NavType.DROPDOWN
+     */
+    public void setNavType(Activity activity, NavType type){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Gets the NavTypes of an Activity
+     * @param activity The Activity to set the NavType on
+     * @return an Arraylist of NavTypes
+     */
+    public ArrayList<NavType> getNavTypes(Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     * THIS METHOD WILL BE INTENSE - REQUIRES DESIGN OF KEYBOARD INPUT TO MAKE
+     * CUSTOM AccessibilityHandlers
+     * Sets the Activity to be keyboard accessible
+     * @param activity The Activity to set
+     */
+    public void setKeyboardAccessible(Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets the a custom KeyboardAccessibilityHandler
+     * @param activity The Activity to set the handler on
+     * @param handler the custom handler to add
+     */
+    public void setKeyboardAccessibleHandler(Activity activity, AccessibilityHandler handler){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets the all taps to single taps
+     * @param activity The Activity to set
+     */
+    public void setActivitySingleTaps(Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets the grace period on all taps
+     * @param activity The Activity to set
+     * @param sec The number of seconds for the grace period
+     */
+    public void setTapGracePeriod(Activity activity, int sec){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets the all gestures to single handed gestures
+     * @param activity The Activity to set
+     */
+    public void setSingleHandGestures(Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets the input time for a View
+     * @param view The View to set
+     * @param sec The number of seconds for the input
+     * @param disable The boolean to determine if time can be disabled
+     * @param extend The boolean to determine if time can be extended
+     */
+    public void setTimeInput(View view, int sec, boolean disable, boolean extend){
+        throw new RuntimeException("Method not implemented");
+    }
+
+
+    /**
+     *
+     * Sets the minimum padding on Views in an Activity
+     * @param activity The Activity to set
+     * @param min The minimum number of padding (px)
+     */
+    public void setMinimumPadding(Activity activity, int min){
+        throw new RuntimeException("Method not implemented");
+    }
 }

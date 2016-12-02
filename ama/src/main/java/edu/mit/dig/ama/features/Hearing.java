efd957fc4eed32b8f793196672b96177e53f15c4
Byungkyu Park <bkpark@mit.edu>
2016-12-02 12:06:34 -0500

    added Hearing and Voice helper functions

--- ama/src/main/java/edu/mit/dig/ama/features/Hearing.java
+++ ama/src/main/java/edu/mit/dig/ama/features/Hearing.java
@@ -1,8 +1,55 @@
 package edu.mit.dig.ama.features;
 
+import android.app.Activity;
+import android.view.View;
+
 /**
- * Created by Vontell on 11/28/16.
+ * Hearing related accessibility helper functions.
  */
 
 public class Hearing {
+
+    /**
+     *
+     * Get to the "No Audio" mode by disabling all the parts
+     * that contain audio prompts
+     *
+     * @param view the view to check upon
+     */
+    public void getNoAudioMode(View view){
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     *
+     * Check if the current view contains any audio related features
+     *
+     * @param view the view to check upon
+     */
+    public void checkIfAudioExists(View view){
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     *
+     * Add visual feedback to the current view
+     * Ideally, this method would be used in place of audio feedback
+     * where there does not exist any non-audio feedback
+     *
+     * @param view the view to check upon
+     * @param activity the Activity to set feedback on
+     */
+    public void addVisualFeedback(View view, Activity activity){
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     *
+     * Check if any feedback is audio-only
+     *
+     * @param view the view to check upon
+     */
+    public void checkFeedbackOnlySpeech(View view){
+        throw new RuntimeException("Method not implemented");
+    }
 }

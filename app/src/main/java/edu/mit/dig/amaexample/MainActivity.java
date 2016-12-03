0fc6d29a1f28335863792db65e24406777531b64
Aaron Vontell <vontell@mit.edu>
2016-12-03 15:17:54 -0500

    Documentation on MainActivity of demo app

--- app/src/main/java/edu/mit/dig/amaexample/MainActivity.java
+++ app/src/main/java/edu/mit/dig/amaexample/MainActivity.java
@@ -3,6 +3,20 @@ package edu.mit.dig.amaexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Main activity for the demo app of Accessiblity for Mobile Applications
+ *
+ * Features of this app:
+ *      - Provides ActionClass button types
+ *      - Custom TalkBack speaking
+ *      - Check if a string is Accessible
+ *      - Speak back when orientation changed
+ *
+ * @author Aaron Vontell
+ * @author William Caruso
+ * @author Byungkyu Park
+ * @version 12.3.2016
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override

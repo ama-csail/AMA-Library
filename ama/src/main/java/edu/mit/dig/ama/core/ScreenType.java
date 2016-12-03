e8e40afe481091309743043c7f722b72d89022f1
Aaron Vontell <vontell@mit.edu>
2016-12-03 14:40:12 -0500

    More vision specs

---
+++ ama/src/main/java/edu/mit/dig/ama/core/ScreenType.java
@@ -0,0 +1,8 @@
+package edu.mit.dig.ama.core;
+
+/**
+ * Created by Vontell on 12/3/16.
+ */
+
+public enum ScreenType {
+}
---
+++ ama/src/main/java/edu/mit/dig/ama/features/Core.java
@@ -0,0 +1,8 @@
+package edu.mit.dig.ama.features;
+
+/**
+ * Created by Vontell on 12/3/16.
+ */
+
+public class Core {
+}

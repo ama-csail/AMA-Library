f17736432d9c4acee1216b272e12b0f54f7d4ac0
Aaron Vontell <vontell@mit.edu>
2017-01-01 02:13:33 -0500

    Remove unused import

--- ama/src/main/java/edu/mit/dig/ama/core/AMA.java
+++ ama/src/main/java/edu/mit/dig/ama/core/AMA.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.net.Uri;
 import android.support.annotation.ColorRes;
 import android.support.annotation.LayoutRes;
